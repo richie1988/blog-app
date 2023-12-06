c14fd3d1a49cd89db21954caeb6f230ac48b427e
richie1988 <richiessikaonga@gmail.com>
2023-12-06 02:14:59 +0200

    Data in Models implementation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
+
+  def update_user_post_counter
+    author.update(post_counter: author.posts.count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
