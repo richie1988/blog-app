81dead72ae3e205a2f5a869cea0f79507e4fc41a
richie1988 <richiessikaonga@gmail.com>
2023-12-11 15:23:18 +0200

    Added controllers for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  root 'articles#index'
-  get '/articles', to: 'articles#index'
+  root 'users#index'
+  get 'posts/:id', to: 'posts#show', as: 'post'
+  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get 'users/:id', to: 'users#show', as: 'user'
+
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'users/index'
+  # get 'users/show'
+  # root 'articles#index'
+  # get '/articles', to: 'articles#index'
 end
