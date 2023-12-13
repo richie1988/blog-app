5a79c6bc0ea48a4a6f40034b88cacbdff5ad0662
richie1988 <richiessikaonga@gmail.com>
2023-12-13 15:31:18 +0200

    Created views foe each post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 # app/controllers/posts_controller.rb
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,8 @@ class Post < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  after_create :update_user_posts_counter
+
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
   end
