a65bf0e337567db04e9851247a3a00380d2d91b6
richie1988 <richiessikaonga@gmail.com>
2023-12-20 16:24:47 +0200

    Implementing the login authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
+  # root 'users#index'
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
